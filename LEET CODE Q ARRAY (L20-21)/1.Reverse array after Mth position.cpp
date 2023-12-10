6b79860e8aa5b23592461a7a5b935791e1e423a5
Rutujabhonde18 <145857690+Rutujabhonde18@users.noreply.github.com>
2023-12-10 22:44:44 +0530

    Update 1.Reverse array after Mth position.cpp

--- LEET CODE Q ARRAY (L20-21)/1.Reverse array after Mth position.cpp
+++ LEET CODE Q ARRAY (L20-21)/1.Reverse array after Mth position.cpp
@@ -1,1 +1,12 @@
+void reverseArray(vector<int> &arr , int m)
+{    
+    int s = 0,  e = arr.size()-1;
 
+        while(s <= e )
+
+        {
+
+                swap(arr[s++],arr[e--]);
+
+        }
+}
