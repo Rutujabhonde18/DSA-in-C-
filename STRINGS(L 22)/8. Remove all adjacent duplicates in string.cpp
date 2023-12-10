41748e582a2755f05d6d9f5248c438d073365dbd
Rutujabhonde18 <145857690+Rutujabhonde18@users.noreply.github.com>
2023-12-10 22:37:58 +0530

    Update 8. Remove all adjacent duplicates in string.cpp

--- STRINGS(L 22)/8. Remove all adjacent duplicates in string.cpp
+++ STRINGS(L 22)/8. Remove all adjacent duplicates in string.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string unique = "";
+        int index = 0;
+        while(index < s.length())
+        {
+            if(!unique.empty()  && unique.back() == s[index])
+            {
+                unique.pop_back();
+            }
+            else
+            {
+                unique.push_back(s[index]);
+            }
+            index++;
+        }
+        return unique;
+    }
+};
