b343bc6e6c31248afa274518d411a2f7566dbf6d
Rutujabhonde18 <145857690+Rutujabhonde18@users.noreply.github.com>
2023-12-12 21:29:48 +0530

    Update 1. Intro pointers.cpp

--- POINTERS/1. Intro pointers.cpp
+++ POINTERS/1. Intro pointers.cpp
@@ -27,6 +27,55 @@ int main()
 	cout<<"Size of integer = "<<sizeof(num)<<endl;
 	cout<<"Size of integer = "<<sizeof(ptr)<<endl;
 	cout<<"Size of integer = "<<sizeof(p2)<<endl;
+
+	//Bad Practice
+	//Pointer to int is created and pointing to some garbage address
+	int *p;
+	cout<<*p<<endl;
+	
+	//Segmentation fault -> Null ahe that's why
+	int *p2 = 0;
+	cout<<*p2<<endl;
+	
+	int i=5;
+	//both give same output
+	int *q = &i;
+	cout<<q<<endl;
+	cout<<*q<<endl;
+	
+	int *p = 0;
+	p = &i;
+	cout<<p<<endl;
+	cout<<*p<<endl;
+	
+	//increment num
+	int num = 5;
+	int a = num;
+	cout<<"Before "<<num<<endl;
+	a++;
+	cout<<"After "<<num<<endl;  	//-> not increment
+	
+	int *p = &num;
+	cout<<"Before "<<num<<endl;
+	(*p)++;
+	cout<<"After "<<num<<endl;		//-> increment
+	
+	//copint pointer
+	
+	int *q = p;
+	cout<<p<<" - "<<q<<endl;
+	cout<<*p<<" - "<<*q<<endl;
+	
+	//Arithmetic Addition
+	int i=3;
+	int *t = &i;
+	//cout<<(*t)++<<endl;	-> Not increment
+	*t = *t +1;				//-> Not increment
+	cout<<*t<<endl;
+	
+	cout<<t<<endl;
+	t = t +1;				
+	cout<<t<<endl;
 	
 	return 0;
 }
