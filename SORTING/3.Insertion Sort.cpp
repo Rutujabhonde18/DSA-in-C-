213779abaa66ad3f36edb4d9b516c40d3e88df1d
Rutujabhonde18 <145857690+Rutujabhonde18@users.noreply.github.com>
2023-12-20 11:23:33 +0530

    Update 3.Insertion Sort.cpp

--- SORTING/3.Insertion Sort.cpp
+++ SORTING/3.Insertion Sort.cpp
@@ -1,1 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+void insertionSort(int arr[], int n)
+{
+	for(int i=1; i<n; i++)
+	{
+		int temp = arr[i];
+		int j=i-1;
+		for(; j>=0; j--)
+		{
+			if(arr[j] > temp)
+			{
+				arr[j+1] = arr[j];
+			}
+			else
+			{
+				break;
+			}
+		}
+		arr[j+1] = temp;
+	}
+}
+
+int main()
+{
+	int arr[5] = {33,11,32,67,43};
+	insertionSort(arr, 5);
+	
+	for(int i=0; i<5; i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+	
+	return 0;
+} 
