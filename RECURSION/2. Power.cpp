ebe76802409e64abaa29efe0d768797b865e8201
Rutujabhonde18 <145857690+Rutujabhonde18@users.noreply.github.com>
2024-01-04 13:26:11 +0530

    Update 2. Power.cpp

--- RECURSION/2. Power.cpp
+++ RECURSION/2. Power.cpp
@@ -21,39 +21,3 @@ int main()
 		
 	return 0;
 } 
-
-
-------------------------------------------
-    Head Recursion ->  1 - n
-------------------------------------------
-
-void print(int n)
-{
-	//base case
-	if(n == 0){
-		return ;
-	}
-	
-	//recursive relation 
-	print(n-1);
-	
-	cout<<n<<endl;
-}
-
-
-------------------------------------------
-    Tail Recursion ->  n - 1
-------------------------------------------
-
-void print(int n)
-{
-	//base case
-	if(n == 0){
-		return ;
-	}
-	
-	cout<<n<<endl;
-	//recursive relation 
-	print(n-1);
-	
-}
