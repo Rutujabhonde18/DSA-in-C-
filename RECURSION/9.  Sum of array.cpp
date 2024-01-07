7c9aef98e7ceebbf954eedc25061bb7b41ae1c17
Rutujabhonde18 <145857690+Rutujabhonde18@users.noreply.github.com>
2024-01-07 22:57:45 +0530

    Create 9.  Sum of array.cpp

---
+++ RECURSION/9.  Sum of array.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int getSum(int *arr, int size)
+{
+	//base case
+	if(size == 0){
+		return 0;
+	}
+		
+	if(size == 1){
+		return arr[0];
+	}
+		
+	int rp = getSum(arr+1, size-1);
+	int sum = arr[0] + rp;
+	return sum;
+}
+
+int main()
+{
+	int arr[5] = {2,4,6,10,9};
+	int size =  5;
+	
+	int sum = getSum(arr, size);
+	
+	cout<<"Sum = "<<sum;
+	
+	return 0;
+}
